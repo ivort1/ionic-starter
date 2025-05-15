988492599ec310d8f89a60dd5b2bb1684db7a35e
ivort1 <ivan.ortiz1@ingka.ikea.com>
2025-05-14 20:11:22 -0700

    Added real photos

--- src/components/atoms/Service/Service.tsx
+++ src/components/atoms/Service/Service.tsx
@@ -4,12 +4,12 @@ interface ServiceProps {
   service: string;
   details: string;
   price: string;
-  index: string;
+  key: number;
 }
 
-export default function Service({ service, details, price, index }: ServiceProps) {
+export default function Service({ service, details, price, key }: ServiceProps) {
   return (
-    <div className="service-component">
+    <div className="service-component" key={key}>
         <div>
             <h2 className="service-component--service">{service}</h2>
             <p className="service-component--details">{details}</p>
