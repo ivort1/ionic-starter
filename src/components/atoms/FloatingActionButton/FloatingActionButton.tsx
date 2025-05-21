d16178ff66ead5c0f5b66421fa1695e65fde390e
ivort1 <ivan.ortiz1@ingka.ikea.com>
2025-05-21 10:45:20 -0700

    Adjusted FAB and added _redirects file

--- src/components/atoms/FloatingActionButton/FloatingActionButton.tsx
+++ src/components/atoms/FloatingActionButton/FloatingActionButton.tsx
@@ -9,7 +9,7 @@ interface FloatingActionButtonProps {
 export default function FloatingActionButton({ href, icon, text }: FloatingActionButtonProps) {
   return (
     <a href={href} className="floating-action-button" target="_blank" rel ="noopener noreferrer">
-      { icon } { text }
+      {icon} {text}
     </a>
   );
 }
